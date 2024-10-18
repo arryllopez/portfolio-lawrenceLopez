37e3c7f826715023f4cdf54d2b683a5157d18242
arryllopez <arryllopez7@gmail.com>
2024-10-18 16:20:58 -0400

    centered hidden text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
-import Flowers from '../assets/flowers.jpg'
-import Cat from '../assets/cat.jpg'
-import House from '../assets/house.jpg'
-import Road from '../assets/road.jpg'
+
+import comingSoon from '../assets/comingSoon.jpg'
 
 const Projects = () => {
   return (
@@ -13,10 +11,10 @@ const Projects = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
         <div className = 'grid sm:grid-cols-2 gap-12 m:grid-col'>
-          <ProjectItem img = {Flowers} title = 'Flowers'/> 
-          <ProjectItem img = {Cat} title = 'Cat'/> 
-          <ProjectItem img = {House} title = 'House'/> 
-          <ProjectItem img = {Road} title = 'Road'/> 
+          <ProjectItem img = {comingSoon} title = 'Coming Soon'/> 
+          <ProjectItem img = {comingSoon} title = 'Coming Soon'/> 
+          <ProjectItem img = {comingSoon} title = 'Coming Soon'/> 
+          <ProjectItem img = {comingSoon} title = 'Coming Soon'/> 
           </div> 
     </div>
   )
