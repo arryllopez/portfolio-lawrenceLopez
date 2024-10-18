19214f3fb14f00cd92915597bcc5e5a19e31500e
arryllopez <arryllopez7@gmail.com>
2024-10-18 12:27:36 -0400

    created project and projectitems

---
+++ src/components/Projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProjectItem from './ProjectItem'
+
+
+const Projects = () => {
+  return (
+    <div id = 'projects' className  = 'max-w-[1040px] m-auto md:pl-20 p-4 py-16' >
+        <h1 className = 'text-4xl font-bold text-center text-[#001b5e]'>Projects</h1> 
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    
+    </div>
+  )
+}
+
+export default Projects
