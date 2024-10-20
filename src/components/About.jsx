d1b493257e21684cc5947199f8e88fb9a2e61cab
arryllopez <arryllopez7@gmail.com>
2024-10-19 22:51:06 -0400

    added coming soon photos and a short summary about myself

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {motion} from 'framer-motion'
+import ComingSoon from '../assets/comingSoon.jpg'
 
 //initializing index values for framer motion staggered animation
 var index = 1
@@ -24,7 +25,6 @@ const fadeInAnimationVariants =
 const aboutMe = () => {
   return (
     <div id = 'about' className  = 'max-w-[1040px] m-auto md:pl-20 p-4 py-16' >
-  
       <motion.h1 
       className = 'text-4xl font-bold text-center text-[#cc5500]'
       variants = {fadeInAnimationVariants}
@@ -44,9 +44,18 @@ const aboutMe = () => {
           once : true,
         }}
         custom = {index2}
-      className = 'text-align:left py-8'> 
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad m
+      className = 'text-align:center py-8'> 
+      Hello, my name is Lawrence Arryl Lopez. I am a second-year Software Engineering student at Ontario Tech University, who is very passionate about technology and problem-solving. 
+      I thoroughly enjoy coding computer software and I love the challenge of modifying and programming pieces of technological hardware. 
+      Moreove,r when I am not immersed in tech, you can find me on the basketball court, strategizing over a game of chess, or enjoying the outdoors through fishing and cycling. 
+      Throughout my everyday life, I always seek new opportunities to broaden my knowledge about coding and to tackle new exciting projects!
       </motion.p> 
+
+      <div className = 'grid sm:grid-cols-2 gap-12 m:grid-col py-8'>
+        <img className = 'rounded-xl group-hover:opacity-10 shadow-xl shadow-gray-400 hover:scale-105' src = {ComingSoon} alt = "/" />
+        <img className = 'rounded-xl group-hover:opacity-10 shadow-xl shadow-gray-400 hover:scale-105' src = {ComingSoon} alt = "/" />
+      
+      </div> 
     </div>
   )
 }
